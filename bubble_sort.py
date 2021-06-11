1888480a8c1366e43852c103ccdc56f842357afc
IB Teguh <teguh.teja@gmail.com>
2021-06-11 11:57:08 +0800

    first commit

---
+++ bubble_sort.py
@@ -0,0 +1,25 @@
+# Python program for implementation of Bubble Sort
+  
+def bubbleSort(arr):
+    n = len(arr)
+  
+    # Traverse through all array elements
+    for i in range(n-1):
+    # range(n) also work but outer loop will repeat one time more than needed.
+  
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+  
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1] :
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+  
+# Driver code to test above
+arr = [6, 3, 1, 8, 7, 2]
+  
+bubbleSort(arr)
+  
+print ("Sorted array is:")
+print(arr)
