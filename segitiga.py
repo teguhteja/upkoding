2a32266dd3ecd8ae2f448df9217108ab843ca45a
IB Teguh <teguh.teja@gmail.com>
2021-06-11 12:59:52 +0800

    fix segitiga

--- segitiga.py
+++ segitiga.py
@@ -1,16 +1,17 @@
 def segitiga(number):
     t = int(number/2)
     for i in range(number):
-        start = t if i == 0 else t - i / 2
+        start = t if i == 0 else int(t - i / 2)
         end =  t if i == 0 else t + i / 2
         for j in range(number):
-            if i % 2 == 1:
-                print(' *', end='')
-            else :
-                if start <= j <= end:
+            if start <= j <= end:
+                if i % 2 == 1:
+                    print(' *', end='')
+                else:
                     print('* ', end='')
-                else :
-                    print('  ', end='') 
-        print(f'{start, end, i%2}')
+            else:
+                print('  ', end='')
+        print() 
+        # print(f'{start, end, i%2}')
 
-segitiga(5)
+segitiga(10)
