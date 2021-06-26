622b411d5451e663736d38919657496b7d01d4c6
IB Teguh <teguh.teja@gmail.com>
2021-06-26 20:50:39 +0800

    monk and rotation

---
+++ test/monk_and_rotation_test.py
@@ -0,0 +1,34 @@
+# 1. change the name files
+import monk_and_rotation as app
+
+
+def my_app(my_input, my_output, my_test):
+    # write your input
+    input_values = my_input
+    output = []
+
+    def mock_input(s):
+        # output.append(s)
+        return input_values.pop(0)
+    app.input = mock_input
+    app.print = lambda s: output.append(s)
+
+    my_test()
+
+    # write your output
+    assert output == my_output
+
+
+def read_files(file):
+    with open(file) as f:
+        contents = f.read()
+        f.close()
+    return contents.split('\n')
+
+
+def test_app1():
+    #2 edit input and output files. set output in apps in string
+    #3 change name files input and output
+    input = read_files('i/mar.in')
+    output = read_files('o/mar.ou')
+    my_app(input, output, app.main)
